430ec28588b2e5070fc01febdddef972ec0c782e
dyuganyuk <dyuganyuk@ntdev.ru>
2022-12-24 11:38:41 +0300

    [RSG] overlay-menu

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("button").click((e) => {
-        console.log(e)
-        $('.overlay-menu').show()
+    //Overlay-menu
+    let overlayMenu = $('.overlay-menu');
+
+    $(window).click(function () {
+        if (overlayMenu.hasClass('active')) {
+            $(overlayMenu).removeClass('active');
+        }
     });
+
+    $("#nav-more").click((e) => {
+        e.stopPropagation();
+        $(overlayMenu).toggleClass('active');
+    });
+    $(overlayMenu).click(e => {
+        e.stopPropagation();
+    })
+
+    //Choose-site modal
 
 
 });
